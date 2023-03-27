d1df5dd6161a0f068805b1e0ae44eeb089adac7b
NicoSimpson <nsp0006@alu.medac.es>
2023-03-27 10:40:12 +0200

    factorial commit

--- com.entornos.NicolasSimpsonPadilla.algoritmos/src/com/entornos/NicolasSimpsonPadilla/algoritmos/algoritmos.java
+++ com.entornos.NicolasSimpsonPadilla.algoritmos/src/com/entornos/NicolasSimpsonPadilla/algoritmos/algoritmos.java
@@ -2,4 +2,23 @@ package com.entornos.NicolasSimpsonPadilla.algoritmos;
 
 public class algoritmos {
 
+
+	public static int fibonacci(int n) {
+		int fibo1=1;
+		int fibo2=1;
+		for (int i=2;i<=n;i++) {
+			fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+		}
+		return fibo2;
+	}
+	
+	public static int factorial (int n) {
+		int factorial = 1;
+		for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+		return n;
+	}
+	
 }
