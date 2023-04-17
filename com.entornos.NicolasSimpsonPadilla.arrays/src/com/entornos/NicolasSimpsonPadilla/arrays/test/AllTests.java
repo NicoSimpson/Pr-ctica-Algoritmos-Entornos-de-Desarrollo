087091b175225e9e3d727db25f758c19b9b23fe5
NicoSimpson <nsp0006@alu.medac.es>
2023-04-17 12:32:55 +0200

    Ultimo commit

---
+++ com.entornos.NicolasSimpsonPadilla.arrays/src/com/entornos/NicolasSimpsonPadilla/arrays/test/AllTests.java
@@ -0,0 +1,10 @@
+package com.entornos.NicolasSimpsonPadilla.arrays.test;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ testMisArrayas.class })
+public class AllTests {
+
+}
