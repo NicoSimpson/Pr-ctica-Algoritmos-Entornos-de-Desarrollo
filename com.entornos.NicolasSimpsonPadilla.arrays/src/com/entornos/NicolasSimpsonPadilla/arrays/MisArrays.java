9989fbf9bf8565bd3cb2640f5e3c4d7fe18d9841
NicoSimpson <nsp0006@alu.medac.es>
2023-04-17 11:22:11 +0200

    Segundo ejercicio commit

--- com.entornos.NicolasSimpsonPadilla.arrays/src/com/entornos/NicolasSimpsonPadilla/arrays/MisArrays.java
+++ com.entornos.NicolasSimpsonPadilla.arrays/src/com/entornos/NicolasSimpsonPadilla/arrays/MisArrays.java
@@ -1,5 +1,49 @@
 package com.entornos.NicolasSimpsonPadilla.arrays;
 
-public class MisArrays {
+public abstract class MisArrays {
+
+	/*
+	 * En este ejercicio realizaremos los siguientes calculos:
+	 * - La media de un array
+	 * - La mediana de un array
+	 * - El maximo de un array
+	 * - El minimo de un array
+	 * 
+	 * @version 1.0
+	 * 
+	 * @autor Nicolas Simpson Padilla
+	 * */
+
+	/*calcular la media
+	 * @param media - numero con decimales que ira sumando los valores del array
+	 * @return - devolvemos la media
+	 */
+	
+	public static double media (int[] array) {
+		double media = 0.0;
+		for (int i=0; i < array.length; i++) {
+			 media = media + array[i];
+		}
+		media = media / 2;
+		return media;
+	}
+	
+	/*calcular la mediana
+	 * @param mediana - es el numero que estara en la mitad del array
+	 * @param mitad - es el numero que estara en la mitad del array
+	 * @return - devolvemos la media
+	 */
+	
+	public static double mediana (int[] array) {
+		double mediana;
+		int mitad = array.length / 2;
+		
+		if (array.length % 2 == 0) {
+		 mediana = (array[mitad-1] + array[mitad]) / 2.0;
+		} else {
+		 mediana = array[mitad];
+		}
+		return mediana;
+	}
 
 }
---
+++ com.entornos.NicolasSimpsonPadilla.arrays/src/com/entornos/NicolasSimpsonPadilla/arrays/test/testMisArrayas.java
@@ -0,0 +1,42 @@
+package com.entornos.NicolasSimpsonPadilla.arrays.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.entornos.NicolasSimpsonPadilla.arrays.MisArrays;
+
+class testMisArrayas {
+
+	int array1 [] = new int [] {1,1,1,1,1};
+	int array2 [] = new int [] {1,2,3,4};
+	int array3 [] = new int [] {5,5,5,5};
+	@Test
+	void testMedia() {
+		double resultado1 = MisArrays.media(array1);
+		double resultadoEsperado1 = 2.5;
+		double resultado2 = MisArrays.media(array2);
+		double resultadoEsperado2 = 5;
+		double resultado3 = MisArrays.media(array3);
+		double resultadoEsperado3 = 10;
+		
+		assertEquals(resultadoEsperado1, resultado1);
+		assertEquals(resultadoEsperado2, resultado2);
+		assertEquals(resultadoEsperado3, resultado3);
+	}
+	
+	@Test
+	void testMediana() {
+		double resultado1 = MisArrays.mediana(array1);
+		double resultadoEsperado1 = 1;
+		double resultado2 = MisArrays.mediana(array2);
+		double resultadoEsperado2 = 2.5;
+		double resultado3 = MisArrays.mediana(array3);
+		double resultadoEsperado3 = 5;
+		
+		assertEquals(resultadoEsperado1, resultado1);
+		assertEquals(resultadoEsperado2, resultado2);
+		assertEquals(resultadoEsperado3, resultado3);
+	}
+
+}
